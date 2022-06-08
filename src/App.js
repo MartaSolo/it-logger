aeb6cc665ca9f3c357815b26b9c067071189f178
MartaSolo <marciamarcia@o2.pl>
2022-06-08 22:34:50 +0200

    added dependency, added Logs and LogItem component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import { useEffect, Fragment } from "react";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import SearchBar from "./components/layout/SearchBar";
+import Logs from "./components/logs/Logs";
 import "./App.css";
 
 const App = () => {
-  useEffect = () => {
+  useEffect(() => {
     // initilize Materialize JS
     M.AutoInit();
-  };
+    console.log("useEfectAPP");
+  }, []);
   return (
     <Fragment>
       <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
     </Fragment>
   );
 };
---
+++ src/components/logs/Logs.js
@@ -0,0 +1,67 @@
+import { useState, useEffect } from "react";
+import LogItem from "./LogItem";
+
+const Logs = () => {
+  const [logs, setLogs] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    console.log("useEfect logs");
+    getLogs();
+  }, []);
+
+  const getLogs = async () => {
+    setLoading(true);
+    const res = await fetch("/logs");
+    // console.log("res", res);
+    const data = await res.json();
+    // console.log("data", data);
+    setLogs(data);
+    setLoading(false);
+  };
+
+  if (loading) {
+    return <h4>Loading...</h4>;
+  }
+
+  return (
+    <ul className="collection with-header">
+      <li className="collection-header">
+        <h4 className="center">System Logs</h4>
+      </li>
+      {!loading && logs.length === 0 ? (
+        <p className="center">No logs to show</p>
+      ) : (
+        logs.map((log) => {
+          return <LogItem key={log.id} log={log} />;
+        })
+      )}
+    </ul>
+  );
+};
+
+export default Logs;
+
+//   console.log(logs);
+//   console.log("loading", loading);
+
+//   const getLogs = () => {
+//     fetch(`/logs`, {
+//       method: "GET",
+//       headers: {
+//         Accept: "application/json",
+//       },
+//     })
+//       .then((response) => {
+//         console.log(response);
+//         return response.json();
+//       })
+//       .then((data) => {
+//         setLogs(data);
+//       })
+//       .catch((error) => {
+//         console.log("error", error);
+//       });
+//   };
+
+//   -------------

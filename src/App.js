c739f8441cdda86622d62cc312facba986c59e13
MartaSolo <marciamarcia@o2.pl>
2022-06-08 19:11:26 +0200

    edited index.html and App.js - added materialize

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
 import "./App.css";
 
 const App = () => {
+  useEffect = () => {
+    // initilize Materialize JS
+    M.AutoInit();
+  };
   return <div className="App">My app</div>;
 };
 

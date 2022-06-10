b7f74a55a538f8a7e13e5a3462f5de9f359f7703
MartaSolo <marciamarcia@o2.pl>
2022-06-10 18:50:56 +0200

    added searching logs

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -5,6 +5,7 @@ import {
   ADD_LOG,
   UPDATE_LOG,
   DELETE_LOG,
+  SEARCH_LOGS,
   SET_CURRENT,
   CLEAR_CURRENT,
 } from "./types";
@@ -86,6 +87,25 @@ export const updateLog = (log) => async (dispatch) => {
     const data = await res.json();
     dispatch({
       type: UPDATE_LOG,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data,
+    });
+  }
+};
+
+// search logs
+export const searchLogs = (text) => async (dispatch) => {
+  try {
+    setLoading();
+    const res = await fetch(`/logs?q=${text}`);
+    const data = await res.json();
+
+    dispatch({
+      type: SEARCH_LOGS,
       payload: data,
     });
   } catch (error) {
